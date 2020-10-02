393ba406e1b518dfc81a5f18c5de91d4eaa02330
Nikolay Lazarev <Nikolay.Lazarev@sidenis.com>
2020-10-02 14:44:55 +0300

    add NotNull annotation for numeric field

--- src/main/kotlin/lan/test/demo/kotlinvalidation/TestRequest.kt
+++ src/main/kotlin/lan/test/demo/kotlinvalidation/TestRequest.kt
@@ -1,18 +1,20 @@
 package lan.test.demo.kotlinvalidation
 
+import javax.validation.constraints.NotNull
+
 data class TestRequest(
-        val id: Int,
+        @field:NotNull val id: Int,
         val optionalId: Int?,
         val name: String,
         val optionalName: String?,
-        val value: Double,
+        @field:NotNull val value: Double,
         val optionalValue: Double?,
         val nested: NestedRequest,
         val optionalNested: NestedRequest?
 )
 
 data class NestedRequest(
-        val nestedId: Long,
+        @field:NotNull val nestedId: Long,
         val nestedOptionalId: Long?,
         val nestedName: String,
         val optionalNestedName: String?
