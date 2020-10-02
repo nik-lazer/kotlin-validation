1a086c429fdcb2991c5c70351c5d9e2b651e5216
Nikolay Lazarev <Nikolay.Lazarev@sidenis.com>
2020-10-02 14:04:57 +0300

    fix typos

--- src/main/kotlin/lan/test/demo/kotlinvalidation/PostController.kt
+++ src/main/kotlin/lan/test/demo/kotlinvalidation/PostController.kt
@@ -1,13 +1,13 @@
 package lan.test.demo.kotlinvalidation
 
-import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
 class PostController {
-    @PutMapping("post")
+    @PostMapping("post")
     fun post(@Valid @RequestBody request: TestRequest): TestResponse {
         return toResponse(request)
     }
